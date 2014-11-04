5d3d6437cb26add8cd559b74fa10a53312406186
Mathias Johansson <hej@mathiasjohansson.se>
2014-11-04 22:01:21 +0100

    Removed old files

--- src/main/scala/ChatApp.scala
+++
@@ -1,9 +0,0 @@
-import akka.actor.ActorSystem
-import server._
-
-object ChatApp {
-  def main(args: Array[String]): Unit = {
-    val system = ActorSystem(s"chat-system")
-    system.actorOf(Server.props)
-  }
-}
